07fb5f96d87a49240f814c937436499eb961c292
Shubham Sahane <shubham.sahane@thinkbridge.in>
2024-07-14 02:09:41 +0530

    Initial commit

---
+++ my-angular-app/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,9 @@
+// src/app/hello-world/hello-world.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent { }
